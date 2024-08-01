e872c2ea08a6a73970d8f82500b92d6cc1c1e014
twy8939 <twy8939@naver.com>
2024-08-01 22:52:06 +0900

    feat : add file upload

--- components/file-drag-dropzone.tsx
+++ components/file-drag-dropzone.tsx
@@ -1,10 +1,33 @@
 "use client";
 
+import { Button } from "@material-tailwind/react";
+import { uploadFile } from "actions/storageActions";
+import { useRef } from "react";
+
 export default function FileDragDropZone() {
+  const fileRef = useRef(null);
+
+  const handleSubmitClick = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const file = fileRef.current.files?.[0];
+
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const result = await uploadFile(formData);
+      console.log(result);
+    }
+  };
+
   return (
-    <section className="w-full py-20 border-4 border-dotted border-indigo-700 flex flex-col items-center">
-      <input type="file" />
+    <form
+      onSubmit={handleSubmitClick}
+      className="w-full py-20 border-4 border-dotted border-indigo-700 flex flex-col items-center"
+    >
+      <input type="file" ref={fileRef} />
       <p>파일을 여기에 끌어다 놓거나 클릭하여 업로드하세요.</p>
-    </section>
+      <Button type="submit">파일 업로드</Button>
+    </form>
   );
 }
