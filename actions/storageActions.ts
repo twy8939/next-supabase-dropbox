88d0534e62628ab7f0aec2029400efbc9cb3fe56
twy8939 <twy8939@naver.com>
2024-08-03 02:05:52 +0900

    feat : Add Delete File

--- actions/storageActions.ts
+++ actions/storageActions.ts
@@ -32,3 +32,15 @@ export async function searchFiles(search: string) {
 
   return data;
 }
+
+export async function deleteFile(fileName: string) {
+  const supabase = await createServerSupabaseClient();
+
+  const { data, error } = await supabase.storage
+    .from(process.env.NEXT_PUBLIC_STORAGE_BUCKET)
+    .remove([fileName]);
+
+  handleError(error);
+
+  return data;
+}
--- components/image-item.tsx
+++ components/image-item.tsx
@@ -1,9 +1,21 @@
 "use client";
 
-import { IconButton } from "@material-tailwind/react";
+import { IconButton, Spinner } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { deleteFile } from "actions/storageActions";
+import { queryClient } from "config/ReactQueryClientProvider";
 import { getImageUrl } from "utils/supabase/storage";
 
 export default function ImageItem({ image }) {
+  const deleteFileMutation = useMutation({
+    mutationFn: deleteFile,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["images"] }),
+  });
+
+  const handleDeleteClick = () => {
+    deleteFileMutation.mutate(image.name);
+  };
+
   return (
     <div className="relative w-full flex flex-col gap-2 p-4 border border-gray-100 rounded-2xl shadow-md">
       <div>
@@ -15,8 +27,12 @@ export default function ImageItem({ image }) {
       </div>
       <div>{image.name}</div>
       <div className="absolute top-4 right-4">
-        <IconButton color="red">
-          <i className="fas fa-trash" />
+        <IconButton color="red" onClick={handleDeleteClick}>
+          {deleteFileMutation.isPending ? (
+            <Spinner />
+          ) : (
+            <i className="fas fa-trash" />
+          )}
         </IconButton>
       </div>
     </div>
